e170e1b8123a0f3afda45a88a0c15fad29e25b10
Jaroslav Brtis <6890442+Jardo-51@users.noreply.github.com>
2025-01-10 19:58:44 +0100

    Fixed typo in apple identification.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [1.0.0-rc.2] - [SNAPSHOT]
 
+### Fixed
 
+- Typo in apple identification
 
 ## [1.0.0-rc.1] - 2025-01-10
 
--- src/main/java/com/jardoapps/mf/demo/lib/Apple.java
+++ src/main/java/com/jardoapps/mf/demo/lib/Apple.java
@@ -3,6 +3,6 @@ package com.jardoapps.mf.demo.lib;
 public class Apple implements Fruit {
 	@Override
 	public void identify() {
-		System.out.println("I am an appl!");
+		System.out.println("I am an apple!");
 	}
 }
