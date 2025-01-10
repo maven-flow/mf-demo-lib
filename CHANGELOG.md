7139b5044d2a028ede59dee4a9adf504dcef91b1
Jaroslav Brtis <6890442+Jardo-51@users.noreply.github.com>
2025-01-10 19:42:06 +0100

    Added cranberry.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [1.1.0] - [SNAPSHOT]
 
+### Added
 
+- Cranberry
 
 ## [1.0.0-rc.1] - 2025-01-10
 
---
+++ src/main/java/com/jardoapps/mf/demo/lib/Cranberry.java
@@ -0,0 +1,8 @@
+package com.jardoapps.mf.demo.lib;
+
+public class Cranberry implements Fruit {
+	@Override
+	public void identify() {
+		System.out.println("I am a cranberry!");
+	}
+}
